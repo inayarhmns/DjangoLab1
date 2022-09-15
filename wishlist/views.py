1db494637fa265f3c0122c2638cc2733a59ecade
inaya.rahmanisa <inaya.rahmanisa@gmail.com>
2022-09-15 12:51:41 +0700

    Mengubah file urls.py dan views.py sesuai instruksi lab 2

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
 from wishlist.models import BarangWishlist
+
 # Create your views here.
 def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
@@ -9,3 +11,20 @@ def show_wishlist(request):
     'nama': 'Inaya'
 }
     return render(request, "wishlist.html", context)
+
+def show_xml(request):       
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    if (request == 'xml'):
+        return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    else:
+        return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
